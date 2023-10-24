57c651ec78c014573b39f521f362f83e4e5cda81
2º ASIR - Alejandro Sánchez Collado <asanchezc168@informatica.iesvalledeljerteplasencia.es>
2023-10-24 20:57:19 +0200

    commit

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,7 @@
 def pedir_numero():
-    pregunta = input("Introduce un número entero del 1 al 10: ")
+    n = input("del 1 al 10: ")
 
-    
+    if 1 < n > 10:
+        print("El número debe estar comprendido entre el 1 y el 10")
+
+print(pedir_numero())
