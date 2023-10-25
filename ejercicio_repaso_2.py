2ab6d337f1b04e2ed976d7cc05bab59c7d8783bc
2º ASIR - Alejandro Sánchez Collado <asanchezc168@informatica.iesvalledeljerteplasencia.es>
2023-10-25 20:08:22 +0200

    commit

---
+++ ejercicio_repaso_2.py
@@ -0,0 +1,9 @@
+def contar_alumnos():
+        
+    with open("alumnos.txt", "r") as archivo:
+        lineas = archivo.readlines()
+        numero_de_alumnos = len(lineas)
+        print(f"\nEl número de alumnos es: {numero_de_alumnos}")
+
+# Llamar a la función y mostrar el resultado
+contar_alumnos()
