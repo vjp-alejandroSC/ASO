660ce9ed303c9c2028e6647491a6fd771eb53599
2º ASIR - Alejandro Sánchez Collado <asanchezc168@informatica.iesvalledeljerteplasencia.es>
2023-10-31 20:30:17 +0100

    commit

--- ejercicios_repaso/ejercicio3.py
+++ ejercicios_repaso/ejercicio3.py
@@ -1,12 +1,23 @@
-def eliminar_alumno():
-    alumno = input("Ingrese el nombre del alumno que quiere eliminar: ")
+def eliminar_alumno(nombre):
+    try:
+        with open("ejercicios_repaso/alumnos.txt", "r") as archivo:
+            lineas = archivo.readlines()
+        encontrado = False
 
-    with open("ejercicios_repaso/alumnos.txt", "w+") as archivo:
-        
-        leer_archivo = archivo.readlines()
+        with open ("ejercicios_repaso/alumnos.txt", "w") as archivo:
+            for linea in lineas:
+                if nombre not in linea:
+                    archivo.write(linea)
+                else:
+                    encontrado = True
+            if encontrado:
+                print(f"El alumno {nombre} ha sido borrado.")
+            else:
+                print(f"El alumno {nombre} no fue encontrado en el archivo.")
+    except FileNotFoundError:
+        print("El fichero 'alumnos.txt' no existe")
+
+nombre_eliminar = input("Introduce el nombre del alumno que deseas eliminar: ")
+
+eliminar_alumno(nombre_eliminar)
             
-        if alumno in leer_archivo:
-            leer_archivo[alumno].remove
-            print("El nombre ha sido eliminado.")
-
-eliminar_alumno()            
